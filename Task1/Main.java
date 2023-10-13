4f8e38cffa43fb6300f9df3ae5057f2aeb60b94b
Yogendra Singh <140152059+YogendraSingh161110350@users.noreply.github.com>
2023-10-13 15:51:38 +0530

    Create Main.java

---
+++ Task1/Main.java
@@ -0,0 +1,16 @@
+
+package game.numbergame;
+
+/**
+ *
+ * @author yogendra singh
+ * this is the  main class which is contained main method
+ * 
+ */
+public class MainClass 
+{
+    public static void main(String[] args) 
+    {
+        new GetUserInforamtion();
+    }
+}
